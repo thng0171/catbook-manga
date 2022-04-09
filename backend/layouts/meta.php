b4d07ec2b86d626b2bdfd6a9545a5d6dd9588cc1
error171 <dqngthng@gmail.com>
2022-04-09 16:39:32 +0700

    thêm 1 số page và chỉnh sửa cấu trúc thư mục

---
+++ backend/layouts/meta.php
@@ -0,0 +1,13 @@
+<!-- Meta chung -->
+<meta charset="utf-8" />
+<!-- End: Meta chung -->
+
+<!-- Meta hỗ trợ SEO cho Google -->
+<?php if ($CURRENT_PAGE == "backend.index"): ?>
+  <meta name="keywords" content="Truyện tranh đọc truyện manga" /> 
+  <meta name="description" content="Đọc truyện tranh manga mới nhất" />
+<?php elseif ($CURRENT_PAGE == "backend.shop_categories.index"): ?>
+  <meta name="keywords" content="truyen tranh" /> 
+  <meta name="description" content="Tự do CRUD (Xem, Thêm, Sửa, Xóa)..." />
+<?php endif; ?>
+<!-- End: Meta hỗ trợ SEO cho Google -->
---
+++ backend/layouts/scripts.php
@@ -0,0 +1,2 @@
+<!-- JQuery -->
+<script src="/assets/vendor/jquery/jquery-3.6.0.min.js"></script>
