c4cdbf5182d90b555e4a6ac2b6f57975a500a168
error171 <dqngthng@gmail.com>
2022-04-10 20:09:52 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}" ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#264653',
+        secondary: '#2A9D8F',
+        'c-yellow': '#E9C46A',
+        'c-orange':'#F4A261',
+        'c-red':'#E76F51',
+        
+      },
+    },
+    
   },
-  plugins: [],
+  plugins: [
+  ],
 }
