a2c93b459f6b8d0fbb0d58b6872bd181bc85fc0f
error171 <dqngthng@gmail.com>
2022-04-14 17:30:47 +0700

    Thêm nút và form đăng nhập

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,34 @@ const userButtom = document.querySelector('#user-button');
 userButtom.addEventListener('click', () => {
     userDropdown.classList.toggle('invisible')
 });
-//Menu
-const menu = document.querySelector("#menu");
+//===Menu===
+const menu = document.querySelector("#mobile-menu-3");
 const menuButton = document.querySelector("#menu-button");
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("invisible");
 });
+
+//===Login Modal===
+//set the modal menu element
+const targetEl = document.getElementById('login-modal');
+
+// options with default values
+const options = {
+  placement: 'bottom-right',
+  backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+  onHide: () => {
+      console.log('modal is hidden');
+  },
+  onShow: () => {
+      console.log('modal is shown');
+  },
+  onToggle: () => {
+      console.log('modal has been toggled');
+  }
+};
+const modal = new Modal(targetEl, options);
+
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 module.exports = {
-  content: ["./**/*.{html,js}" ],
+  darkMode: 'class',
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: '#2A9D8F',
         secondary: '#264653',
         'c-yellow': '#E9C46A',
-        'c-orange':'#F4A261',
-        'c-red':'#E76F51',
-        
+        'c-orange': '#F4A261',
+        'c-red': '#E76F51',
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
       },
     },
-    
+
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
+    require('flowbite/plugin')
   ],
 }
