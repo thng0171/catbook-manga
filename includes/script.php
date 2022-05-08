2121fbde05a2489335982678ac2e40a7610aac62
error171 <dqngthng@gmail.com>
2022-05-08 22:06:09 +0700

    make it better

--- includes/script.php
+++ includes/script.php
@@ -2,3 +2,4 @@
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/theme-change@2.0.2/index.js"></script>
 <script src="assets/vendor/show-more/dist/js/showMore.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/tomik23/show-more@1.1.3/dist/js/showMore.min.js"></script>
--- includes/style.php
+++ includes/style.php
@@ -7,3 +7,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,300;1,400;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="assets/vendor/show-more/dist/css/show-more.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tomik23/show-more@1.1.3/dist/css/show-more.min.css" />
